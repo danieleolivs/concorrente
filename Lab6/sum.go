c600e0dbb18ed344a105527652f71dc1ef5820ae
danieleolivs <danieleoliveira7712@gmail.com>
2025-03-25 12:59:19 -0300

    struct

---
+++ Lab6/sum.go
@@ -0,0 +1,26 @@
+func calculaSoma(files []string) map[string]string{
+	sumChan := make(chan struct{path , sum string})
+
+	var wg sync.WaitGroup
+
+	for _, file := range files{
+		g.Add(1)
+		go func(f string){
+			defer wg.Done()
+			sum:= calculaSum(f)
+			sumChan <- struct{path, sum string}{f, sum}
+		}(file)
+	}
+
+	go func(){
+		wg.wait()
+		close(sumChan)
+	}()
+
+	sums := make(map[string]string)
+	for result := range sumChan{
+		sums[result.path] = result.sum
+	}
+
+	return sums
+}
